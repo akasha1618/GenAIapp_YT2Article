65ca89810df4c85680f35daa78ce896e315af08a
PopAlexandru23 <alexandru.pop@intenselife.ch>
2023-12-14 01:41:56 +0200

    YouTube Video to SEO Article Writer\Generator

---
+++ GenAIapp_YT2Article/README.md
@@ -0,0 +1,48 @@
+ YouTube Video to SEO Article Writer\Generator
+
+This project is a web application that generates an article based on the content of a YouTube video. The user enters the URL of a YouTube video, and the application downloads the video, transcribes the audio to text using OpenAI's Whisper API, and generates an article with specific SEO keywords using the LangChain framework.
+
+## Features
+
+- Download YouTube videos using pytube
+- Transcribe audio to text using OpenAI's Whisper API
+- Generate an article based on the video content using LangChain
+
+## Prerequisites
+
+- Python 
+- pip
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/akasha1618/GenAIapp_YT2Article.git`
+2. Navigate to the project directory: `cd yourrepository`
+3. Install the dependencies: `pip install -r requirements.txt`
+
+## Configuration
+
+Create a `.env` file in the root directory of the project, and add the following line:
+
+OPENAI_API_KEY=your_openai_api_key
+
+Replace `your_openai_api_key` with your actual OpenAI API key.
+
+## Usage
+
+1. Start the server: `python app.py`
+2. Open your web browser and navigate to `http://localhost:5000`
+3. Enter the URL of a YouTube video and click the "Write Article" button
+
+## Technologies Used
+
+- Python
+- Flask
+- OpenAI Whisper API
+- LangChain
+- pytube
+- OpenAI gpt-4=turbo
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
---
+++ GenAIapp_YT2Article/app.py
@@ -0,0 +1,79 @@
+from openai import OpenAI
+import whisper
+import os
+from dotenv import load_dotenv
+from langchain.chains import LLMChain
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.agents import AgentType
+from langchain.agents import load_tools, initialize_agent
+from langchain.agents import tool
+import tiktoken
+from pytube import YouTube
+from moviepy.editor import *
+from flask import Flask, redirect, url_for, request, render_template, send_file
+import requests
+
+#OpenAI initialization
+load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+client = OpenAI()
+
+#load the prompt from file
+with open("prompt_article", "r") as file:
+    prompt_article = file.read()
+
+
+app = Flask(__name__)
+
+def download_and_transcribe(url):
+
+    yt = YouTube(url)
+
+    audio_stream = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
+    sanitized_title = yt.title.replace(" ", "_")
+
+    audio_stream.download('.', filename=sanitized_title + '.webm')
+    file_name = f'{sanitized_title}.webm'
+    with open(file_name, "rb") as audio_file:
+        transcript = client.audio.transcriptions.create(model="whisper-1", file=audio_file)
+    return transcript  
+
+def generate_article_from_transcript(transcript):
+    prompt =prompt_article
+    llm = ChatOpenAI(temperature=0.0, model='gpt-4-1106-preview')
+    prompt_template = ChatPromptTemplate.from_template(prompt)
+    chain = LLMChain(llm=llm, prompt=prompt_template)
+    return chain.run(transcript)
+
+#route for the initial index page
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+#route for the generation result page
+@app.route('/generate-article', methods=['POST'])
+def generate_article():
+    youtube_link = request.form['youtube_link']
+    transcription = download_and_transcribe(youtube_link)
+    generated_article = generate_article_from_transcript(transcription)
+    #save article to file
+    filename = "article.txt"
+    with open(filename, 'w') as file:
+        file.write(generated_article)
+
+    return render_template('result.html', article=generated_article)
+
+#route for downloading the generated article
+@app.route('/download-article', methods=['GET'])
+def download_article():
+    return send_file("article.txt", as_attachment = True)
+   
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
